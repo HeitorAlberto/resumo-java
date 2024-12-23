a5396b583d5e7a3e35afa580f54bc68479834f66
HeitorAlberto <56478574+HeitorAlberto@users.noreply.github.com>
2024-12-23 10:51:33 -0300

    Create java02.java

---
+++ java02.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+String nome = JOptionPane.showInputDialog("Digite seu nome: ");
+JOptionPane.showMessageDialog(null, "Bom dia " + nome);
+
+int idade = integer.parseaInt(JOptionPane.showInputDialog("Digite sua idade: ");
+JOptionPane.showMessageDialog(null, "Sua idade é " + idade);
